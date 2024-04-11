7c01af72f2cf35c7dc02c6d84dafb76098bdfc8f
Hypers9 <henrylee1606@gmail.com>
2024-04-10 22:22:58 -0400

    v1.1 Updated Code & Images

--- Realetor JavaScript.js
+++ Realetor JavaScript.js
@@ -1,8 +1,1 @@
 // JavaScript Document
-const hamburgerMenu = document.querySelector('.hamburger-menu');
-const navList = document.getElementById('navbar');
-
-hamburgerMenu.addEventListener('click', () => {
-  navList.classList.toggle('active');
-  hamburgerMenu.classList.toggle('active');
-});
